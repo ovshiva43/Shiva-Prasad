1b1c8c101817174b61dd045bb32e16edbf1cf331
shiva <ovshiva43@gmail.com>
2019-10-12 16:18:51 +1100

    Playing with java script.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
-let myHeading = documents.queryselector('h1');
-myHeading.textContent = "Shiva Prasad Bhandari";
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Shiva Prasad Bhandari!';
