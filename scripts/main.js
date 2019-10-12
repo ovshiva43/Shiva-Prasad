9cf68dd23bf06eca00aac54735a7986dfd568305
shiva <ovshiva43@gmail.com>
2019-10-12 21:34:20 +1100

    images changer in java script

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src')
+    if(mySrc === 'images/shiva.png') {
+        myImage.setAttribute ('src', 'images/shiva-2');
+    } else {
+        myImage.setAttribute ('src', 'images/shiva.png')
+    }
+}
